2ecfdab0356a7cc52b6db89f4116f79a696a6755
paabrilc <123276294+paabrilc@users.noreply.github.com>
2023-03-11 14:11:24 -0600

    Update main.java
    Punto uno administrador

--- Proyecto_Grupo2_-Cajero/src/proyecto_grupo2_/cajero/main.java
+++ Proyecto_Grupo2_-Cajero/src/proyecto_grupo2_/cajero/main.java
@@ -1,26 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package proyecto_grupo2_.cajero;
+import java.util.Scanner;
 
-/**
- *
- * @author cihc2
- */
-public class main {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //This is a test
-        int a = 0,b=5,c=4;
-        int suma = a+b+c;
-        
-        clsHelper clsH = new clsHelper();
-        clsH.imprimeMensaje("Resultado: "+suma);
+        Scanner scanner = new Scanner(System.in);
+        String opcion;
+
+        // pedir al usuario que seleccione el modo
+        System.out.println("Seleccione el modo (administrador/usuario): ");
+        opcion = scanner.next();
+
+        if (opcion.equals("administrador")) {
+            System.out.println("Ingrese su número de identificación: ");
+            String identificacion = scanner.next();
+
+            System.out.println("Cree su contraseña: ");
+            String contrasena = scanner.next();
+
+            int intentos = 0;
+            boolean bloqueado = false;
+
+            // pedir al usuario que ingrese el usuario y la contraseña
+            while (intentos < 3 && !bloqueado) {
+                System.out.println("Ingrese su identificación: ");
+                String identificacionIngresada = scanner.next();
+
+                System.out.println("Ingrese su contraseña: ");
+                String contrasenaIngresada = scanner.next();
+
+                // verificar si la identificación y la contraseña son correctas
+                if (identificacionIngresada.equals(identificacion) && contrasenaIngresada.equals(contrasena)) {
+                    System.out.println("Bienvenido, administrador!");
+                    break;
+                } else {
+                    intentos++;
+                    System.out.println("Identificación o contraseña incorrecta. Por favor, intente de nuevo.");
+
+                    if (intentos == 3) {
+                        System.out.println("Ha ingresado información incorrecta 3 veces. Usted ha sido bloqueado.");
+                        bloqueado = true;
+                    }
+                }
+            }
+        } else if (opcion.equals("usuario")) {
+            System.out.println("Bienvenido, usuario!");
+        } else {
+            System.out.println("Opción inválida.");
+        }
     }
-        
 }
