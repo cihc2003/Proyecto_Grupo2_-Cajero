111bcafb72b759e0d089bfd9802b4effb7835a3a
paabrilc <123276294+paabrilc@users.noreply.github.com>
2023-03-11 13:48:36 -0600

    Add files via upload

---
+++ Proyecto_Grupo2_-Cajero/src/proyecto_grupo2_/cajero/main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_grupo2_.cajero;
+
+/**
+ *
+ * @author cihc2
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //This is a test
+        int a = 0,b=5,c=4;
+        int suma = a+b+c;
+        
+        clsHelper clsH = new clsHelper();
+        clsH.imprimeMensaje("Resultado: "+suma);
+    }
+        
+}
